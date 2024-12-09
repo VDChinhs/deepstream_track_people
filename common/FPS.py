50eaea826c322466898ce788a191eefed08c0f5a
Vũ Đức Chính <chinh_vd@ehicas.vn>
2024-12-09 19:23:32 +0700

    edit save fps

--- common/FPS.py
+++ common/FPS.py
@@ -10,14 +10,17 @@ class GETFPS:
         global frame_count
         self.frame_count=frame_count
         self.stream_id=stream_id
+        self.fps = 0
     def get_fps(self):
         end_time=time.time()
         if(self.is_first):
             self.start_time=end_time
             self.is_first=False
         if(end_time-self.start_time>5):
+            fps = float(self.frame_count)/5.0
             print("**********************FPS*****************************************")
-            print("Fps of stream",self.stream_id,"is ", float(self.frame_count)/5.0)
+            print("Fps of stream",self.stream_id,"is ", fps)
+            self.fps = fps
             self.frame_count=0
             self.start_time=end_time
         else:
